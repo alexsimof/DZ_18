90508208b1f4ea28911f65dfab5c1da673b6f4a7
Alex Simonov <alex.simonof@gmail.com>
2024-02-11 17:15:33 +0200

    add js code

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+let test = 'ami';
+console.log( NaN+test );
+
+let human1 = {
+  firstName: "Aleksandr",
+  nikname: "simon",
+  phone: "0506202139",
+  mail: "alex.simonof@gmail.com",
+  sex: "male",
+  hobbies: "play on computer",
+  admin: true
+};
+
+let human2 = {
+  firstName: "Serg",
+  nikname: "petrov",
+  phone: "0563204565",
+  mail: "serg@gmail.com",
+  sex: "male",
+  hobbies: "cook",
+  admin: false
+};
+
+let human3 = {
+  firstName: "Kate",
+  nikname: "kat",
+  phone: "0785213840",
+  mail: "caty@gmail.com",
+  sex: "woman",
+  hobbies: "paint",
+  user: true
+};
+
+let banda = ['human1', 'human2', 'human3'];
+console.log(banda)
